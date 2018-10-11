004dfa837631cba2a710bec3d8ed7c3c19c99f0b
self-unit <self_unit@hotmail.com>
2018-10-11 13:16:39 +0100

    first commit

---
+++ src/main/java/Deck.java
@@ -0,0 +1,14 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Deck {
+
+    private ArrayList<Card> cards;
+
+    public Deck() {
+        this.cards = new ArrayList<>();
+    }
+
+    public Array getCards() {
+    }
+}
---
+++ src/test/java/DeckTest.java
@@ -0,0 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DeckTest {
+
+    Card card;
+    Deck deck;
+
+    @Before
+    public void before() {
+//        card = new Card();
+        deck = new Deck();
+    }
+
+    @Test
+    public void hasArray() {
+        assertEquals(0, deck.getCards().size());
+    }
+}
